a13fd97392abab0fa1e49a7a671bb4c751650076
Naomi <naomipriest@rocketmail.com>
2020-03-10 17:08:48 -0400

    saving my working code, I have to add more stuff

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,26 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    users: [],
+    userText: ""
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/NaomiPriest97/followers")
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+        
+          users: res.data
+        });
+      })
+      .catch(err => console.log(err.message));
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.users !== this.setState.users) {
+      console.log("new user array");
+      if (this.state.userText === "NaomiTorres") {
+        console.log(this.state.userText);
+        axios
+          .get(`https://api.github.com/users/NaomiPriest97/`)
+          .then(res => {
+            this.setState({
+              users: res.data,
+              userText: "naomi"
+            });
+          })
+          .catch(err => console.log(err.message));
+      }
+    }
+  }
+
+  handleChanges = e => {
+    this.setState({
+      userText: e.target.value
+    });
+  };
+
+  fetchUsers = e => {
+    e.preventDefault();
+    axios
+      .get(`https://api.github.com/users/NaomiPriest97/followers/${this.state.userText}`)
+      .then(res => {
+        this.setState({
+          users: res.data
+        });
+      })
+      .catch(err => console.log(err.message));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Naomi Ruemmely-Torres</h1>
+        <input value={this.state.userText} onChange={this.handleChanges} />
+
+        <button onClick={this.fetchUsers}>Fetch Users</button>
+        
+        <div className="users">
+          {this.state.users.map(user => {
+            return (
+            <div className="usercard">
+              <h2>{user.login}</h2>
+              <img src={user.avatar_url} key={user} alt={user}/>
+              <p>{user.location}</p>
+             
+            </div>
+            );
+        })}
+
+          
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
