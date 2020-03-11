0fbcec47a236b8c09512cd29c99a50f6b5363f15
Naomi <naomipriest@rocketmail.com>
2020-03-10 20:25:47 -0400

    search bar working, now to do mine

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -2,15 +2,32 @@ import React from 'react';
 import './App.css';
 import axios from "axios";
 
-class App extends React.Component {
 
-  state = {
-    users: [],
-    userText: ""
+function searchingFor(userText){
+  return function(x){
+    return x.login.toLowerCase().includes(userText.toLowerCase()) || !userText;
+  }
+}
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      users: [],
+      userText: ''
+    };
+    this.handleChanges = this.handleChanges.bind(this);
+  }
+  
+  handleChanges = e => {
+    this.setState({
+      userText: e.target.value
+    });
   };
 
   componentDidMount() {
-    axios
+    
+      axios
       .get("https://api.github.com/users/NaomiPriest97/followers")
       .then(res => {
         console.log(res.data);
@@ -20,67 +37,39 @@ class App extends React.Component {
         });
       })
       .catch(err => console.log(err.message));
+
+    
+      
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.users !== this.setState.users) {
-      console.log("new user array");
-      if (this.state.userText === "NaomiTorres") {
-        console.log(this.state.userText);
-        axios
-          .get(`https://api.github.com/users/NaomiPriest97/`)
-          .then(res => {
-            this.setState({
-              users: res.data,
-              userText: "naomi"
-            });
-          })
-          .catch(err => console.log(err.message));
-      }
-    }
-  }
+  
 
-  handleChanges = e => {
-    this.setState({
-      userText: e.target.value
-    });
-  };
-
-  fetchUsers = e => {
-    e.preventDefault();
-    axios
-      .get(`https://api.github.com/users/NaomiPriest97/followers/${this.state.userText}`)
-      .then(res => {
-        this.setState({
-          users: res.data
-        });
-      })
-      .catch(err => console.log(err.message));
-  };
-
+ 
   render() {
+     const {users, userText} = this.state;
     return (
+     
       <div className="App">
-        <h1>Naomi Ruemmely-Torres</h1>
-        <input value={this.state.userText} onChange={this.handleChanges} />
-
-        <button onClick={this.fetchUsers}>Fetch Users</button>
         
-        <div className="users">
-          {this.state.users.map(user => {
-            return (
-            <div className="usercard">
-              <h2>{user.login}</h2>
-              <img src={user.avatar_url} key={user} alt={user}/>
-              <p>{user.location}</p>
-             
-            </div>
-            );
-        })}
+          <h1>Naomi Ruemmely-Torres</h1>
+          <input placeholder="Search for followers" value={userText} onChange={this.handleChanges} />
 
           
-        </div>
-      </div>
+          
+          <div className="users">
+            {users.filter(searchingFor(userText)).map((user, index) => {
+              return (
+              <div key={index} className="usercard">
+                <h2>{user.login}</h2>
+                <img src={user.avatar_url} key={user} alt={user}/>
+                <p>Followers - {user.followers_url.length}</p>
+                <p>Following - {user.following_url.length}</p>
+              </div>
+              );
+            })}
+          </div>
+      </div>   
+     
     );
   }
 }
